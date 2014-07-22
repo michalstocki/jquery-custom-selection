7ffc81973448084309fb35ea9b5243c7a7c3b53f
Michał Stocki <mstocki@ydp.eu>
2014-07-22 18:20:02 +0200

    Finding nearest text node: almost working

--- src/js/main.js
+++ src/js/main.js
@@ -39,7 +39,7 @@ $(function() {
 			var markerOffset = mark(markerAnchor, lastPoint, endMarker);
 			if (markerOffset) {
 				endAnchor = markerAnchor;
-				endOffset =  markerOffset;
+				endOffset = markerOffset;
 			}
 			ticking = false;
 		});
@@ -71,26 +71,35 @@ function createSelection() {
 }
 
 function mark(el, point, marker) {
-	var textNode = getNodeFromElByPoint(el, point);
-	if (!textNode) {
+	var textNode;
+	if (textNode = getFromElNodeContainingPoint(el, point)) {
+		while (textNode.length > 1) {
+			var trimPosition = textNode.length >> 1;
+			var subNode = textNode.splitText(trimPosition);
+			if (nodeContainsPoint(subNode, point)) {
+				textNode = subNode;
+			}
+		}
+		putMarkerBefore(textNode, marker);
+	} else if (textNode = getClosestTextNodeFromEl(el, point)) {
+		putMarkerAfter(textNode, marker);
+	} else {
 		return null;
 	}
-
-	while (textNode.length > 1) {
-		var trimPosition = textNode.length >> 1;
-		var subNode = textNode.splitText(trimPosition);
-		if (nodeContainsPoint(subNode, point)) {
-			textNode = subNode;
-		}
-	}
-
-	putMarkerBefore(textNode, marker);
-	el.normalize();
-	return Array.prototype.indexOf.call(el.childNodes, marker);
+	marker.parentNode.normalize();
+	return getIndexOfElement(marker);
 }
 
 function putMarkerBefore(node, marker) {
 	node.parentNode.insertBefore(marker, node);
+}
+
+function putMarkerAfter(node, marker) {
+	if (node.nextSibling) {
+		node.parentNode.insertBefore(marker, node.nextSibling);
+	} else {
+		node.parentNode.appendChild(marker);
+	}
 }
 
 function createMarker(kind) {
@@ -99,9 +108,15 @@ function createMarker(kind) {
 	return span;
 }
 
-// -- Finding nearest text node ------------------------------------------------
+function getIndexOfElement(element) {
+	var elements = element.parentElement.childNodes;
+	return Array.prototype.indexOf.call(elements, element);
+}
 
-function getNodeFromElByPoint(el, point) {
+// -- Finding nearest text node ------------------------------------------------
+// Finding node containing point
+
+function getFromElNodeContainingPoint(el, point) {
 	var nodes = el.childNodes;
 	for (var i = 0, n; n = nodes[i++];) {
 		if (n.nodeType === Node.TEXT_NODE && nodeContainsPoint(n, point)) {
@@ -128,5 +143,64 @@ function getRectsForNode(node) {
 function rectContainsPoint(rect, point) {
 	var x = point.clientX, y = point.clientY;
 	return x > rect.left && x < rect.right && y > rect.top && y < rect.bottom;
+}
+
+function nodeIsText(node) {
+	return node.nodeType === Node.TEXT_NODE && node.length;
+}
+
+// Finding node closest to pointer
+
+function getClosestTextNodeFromEl(el, point) {
+	var node = el;
+	var subNode;
+	while (subNode = getClosestNodeFromEl(node, point)) {
+		if (nodeIsText(subNode)) {
+			return subNode;
+		} else {
+			node = subNode;
+		}
+	}
+	return null;
+}
+
+function getClosestNodeFromEl(el, point) {
+	var nodes = el.childNodes;
+	var closestNode;
+	for (var i = 0, n; n = nodes[i++];) {
+		var rects;
+		if (rects = getRectsForNode(n)) {
+			closestNode = closestNode || n;
+			closestNode = getNodeCloserToPoint(point, closestNode, n);
+		}
+	}
+	return closestNode;
+}
+
+function getNodeCloserToPoint(point, winner, rival) {
+	var nearestWinnerRect = getRectNearestToPoint(winner, point);
+	var nearestRivalRect = getRectNearestToPoint(rival, point);
+	if (nearestRivalRect && nearestWinnerRect &&
+		nearestRivalRect !== nearestWinnerRect &&
+		nearestRivalRect.top >= nearestWinnerRect.top) {
+		return rival;
+	} else {
+		return winner;
+	}
+}
+
+function getRectNearestToPoint(node, point) {
+	var y = point.clientY;
+	var rects = getRectsForNode(node);
+	var nearestRect;
+	for (var j = 0, rect; rect = rects[j++];) {
+		if (rect.top < y) {
+			nearestRect = nearestRect || rect;
+			if (nearestRect !== rect && rect.top >= nearestRect.top) {
+				nearestRect = rect;
+			}
+		}
+	}
+	return nearestRect;
 }
 
