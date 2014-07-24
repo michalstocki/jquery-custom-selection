1a7c9c63e0c79a6f70e1b3c3a3cf808d23556879
Michał Stocki <mstocki@ydp.eu>
2014-07-24 10:05:38 +0200

    RequestAnimationFrame polyfill added to dist.

--- dist/custom-selection.js
+++ dist/custom-selection.js
@@ -1,4 +1,4 @@
-/*! CustomSelection - v0.1.0 - 2014-07-23 */
+/*! CustomSelection - v0.1.0 - 2014-07-24 */
 (function(global) {
 	// Default configuration
 	var startMarkerClass = 'start-marker';
@@ -302,3 +302,32 @@
 	global.CustomSelection.Lib.Point = Point;
 
 })(this);
+
+(function() {
+	var lastTime = 0;
+	var vendors = ['ms', 'moz', 'webkit', 'o'];
+	for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+		window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+		window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] ||
+			window[vendors[x] + 'CancelRequestAnimationFrame'];
+	}
+
+	if (!window.requestAnimationFrame) {
+		window.requestAnimationFrame = function(callback) {
+			var currTime = new Date().getTime();
+			var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+			var id = window.setTimeout(function() {
+					callback(currTime + timeToCall);
+				},
+				timeToCall);
+			lastTime = currTime + timeToCall;
+			return id;
+		};
+	}
+
+	if (!window.cancelAnimationFrame) {
+		window.cancelAnimationFrame = function(id) {
+			clearTimeout(id);
+		};
+	}
+}());
