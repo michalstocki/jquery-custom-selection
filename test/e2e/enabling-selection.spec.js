fe41eb0adb99bad7579b968a79ac0c2bccf24543
Maciej Pieńkowski <mpienkowski@ydp.eu>
2014-10-06 15:43:46 +0200

    EPUB-131: refactored first test, set browser size

--- test/e2e/enabling-selection.spec.js
+++ test/e2e/enabling-selection.spec.js
@@ -3,30 +3,40 @@
 var chai = require('chai');
 var expect = chai.expect;
 
-describe('jquery custom selection', function () {
+describe('jquery custom selection', function() {
 	var URL = 'http://localhost:8004';
+	var BROWSER_SIZE = {width: 1280, height: 1024};
+	var PARAGRAPH_SELECTOR = '#pgepubid00498 p';
+	var ENABLE_SELECTION_BUTTON_SELECTOR = '.button-enable';
+	var MARKER_SELECTOR = PARAGRAPH_SELECTOR + ' span.marker';
+	var SELECTED_TEXT_STATUS_SELECTOR = '.selected-text-status';
+	var MOUSE_DOWN_TIME = 550;
 
-	it('enables selection and selects a specific word in a paragraph', function(done) {
-		var PARAGRAPH_SELECTOR = '#pgepubid00498 p';
-		var ENABLE_SELECTION_BUTTON_SELECTOR = '.button-enable';
-		var MARKER_SELECTOR = PARAGRAPH_SELECTOR + ' span.marker';
-		var SELECTED_TEXT_STATUS_SELECTOR = '.selected-text-status';
-		var MOUSE_DOWN_TIME = 550;
-
-		browser
-			.url(URL)
+	browser.addCommand("initializeAndSelectAWordNearElement", function(click, done) {
+		this.url(URL)
+			.setViewportSize(BROWSER_SIZE)
 			.click(ENABLE_SELECTION_BUTTON_SELECTOR)
-			.moveToObject(PARAGRAPH_SELECTOR, 10, 5)
-			.buttonDown(PARAGRAPH_SELECTOR)
+			.moveToObject(click.elementSelector, click.xOffset, click.yOffset)
+			.buttonDown(click.elementSelector)
 			.pause(MOUSE_DOWN_TIME)
-			.buttonUp(PARAGRAPH_SELECTOR)
+			.buttonUp(click.elementSelector)
 			.isVisible(MARKER_SELECTOR, function(error, result) {
 				expect(error).to.be.undefined;
 				expect(result.length).to.equal(2);
+				done(error);
+			})
+	});
+
+	it('enables selection and selects a specific word in a paragraph', function(done) {
+		browser
+			.initializeAndSelectAWordNearElement({
+				elementSelector: PARAGRAPH_SELECTOR,
+				xOffset: 200,
+				yOffset: 5
 			})
 			.getText(SELECTED_TEXT_STATUS_SELECTOR, function(error, result) {
 				expect(error).to.be.undefined;
-				expect(result).to.equal('The');
+				expect(result).to.equal('classification');
 			})
 			.end(done);
 	});
